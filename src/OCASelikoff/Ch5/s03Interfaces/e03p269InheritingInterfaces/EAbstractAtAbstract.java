ea4fdda91c5c2004e1b68564894961df5473c4c8
Andriy Lyapandra <lyapandra.andriy.s@gmail.com>
2018-04-07 19:16:15 +0300

    Ch 5 is finished

---
+++ src/OCASelikoff/Ch5/s03Interfaces/e03p269InheritingInterfaces/EAbstractAtAbstract.java
@@ -0,0 +1,7 @@
+package OCASelikoff.Ch5.s03Interfaces.e03p269InheritingInterfaces;
+
+/**
+ * Created by a on 07.04.2018.
+ */
+public abstract class EAbstractAtAbstract extends E2HarborSealAbstract implements E1SealInterface{
+}
