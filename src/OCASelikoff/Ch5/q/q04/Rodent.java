8e7827d48c01d7ed16457b83ceddb44fb791e491
Andriy Lyapandra <lyapandra.andriy.s@gmail.com>
2018-04-07 22:58:05 +0300

    added q4 (+)

---
+++ src/OCASelikoff/Ch5/q/q04/Rodent.java
@@ -0,0 +1,21 @@
+package OCASelikoff.Ch5.q.q04;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by a on 07.04.2018.
+ */
+public class Rodent {
+    @NotNull
+//    protected static Integer chew() throws Exception {
+    protected static Number chew() throws Exception {
+        System.out.println("Rodent is chewing");
+        return 1.0f;
+    }
+    public static void main(String[] args) throws Exception {
+        Beaver b =  new Beaver();
+        b.chew();
+        chew();
+    }
+
+}
